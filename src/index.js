9c38fc3801bc925c34160d306685d6d74c8abb08
Shirsho Dipto <shirshodipto@gmail.com>
2023-01-03 22:52:29 +0600

    Eslint

--- src/index.js
+++ src/index.js
@@ -1,122 +1,113 @@
+import mapboxgl from "mapbox-gl";
+import createConflictPopUp from "./popUp";
 
-import mapboxgl from 'mapbox-gl';
-import { createConflictPopUp } from './popUp';
-
-
-function toggleLayer(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    const clickedLayer = e.target.id;
-    const visibility = map.getLayoutProperty(clickedLayer, 'visibility');
-
-    const lines2 = e.target.getAttribute('lines2');
-    console.log(lines2);
-
-    if (visibility === 'visible' || visibility === undefined) {
-        map.setLayoutProperty(clickedLayer, 'visibility', 'none');
-        this.className = '';
-    }
-    else {
-        this.className = 'active';
-        map.setLayoutProperty(clickedLayer,'visibility','visible');
-    }
-}
-
-
-function updateLegends(e) {
-    const legendName = e.target.id;
-    const allLegends = document.querySelectorAll('div[dataId]');
-    for (const legend of allLegends) {
-        if (legend.getAttribute('dataId') !== legendName) {
-            legend.classList.add('hide');
-        }
-        else {
-            legend.classList.remove('hide');
-        }
-    }
-}
-
-
-function displayPopUp(e) {
-    const coordinates = e.features[0].geometry.coordinates.slice();
-    const div = createConflictPopUp(e.features[0].properties);
-
-    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-    }
-
-    new mapboxgl.Popup()
-        .setLngLat(coordinates)
-        .setDOMContent(div)
-        .addTo(map);
-}
-
-
-function changeStyle(layer) {
-    prevLayers = map.style._layers;
-    map.setStyle(layer.target.id);
-    isChangeStyle = 1;
-}
-
-
-// ############## MAIN FUNCTION ###################
-
-const allLayers = document.querySelectorAll('#layers a');
-const viewInputs = document.querySelectorAll('#map-styles input');
+const allLayers = document.querySelectorAll("#layers a");
+const viewInputs = document.querySelectorAll("#map-styles input");
 let prevLayers;
 let isChangeStyle = 0;
-let mapIds = ["conflicted-zones", "oil-pipelines", "gas-pipelines"];
+const mapIds = ["conflicted-zones", "oil-pipelines", "gas-pipelines"];
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoic2hpcnNob2RpcHRvIiwiYSI6ImNsYnlraHR4dDB1Njgzb2xobHZrMG1kY2gifQ.ndu_e63-o0H8MAWnvQ3EWA';
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoic2hpcnNob2RpcHRvIiwiYSI6ImNsYnlraHR4dDB1Njgzb2xobHZrMG1kY2gifQ.ndu_e63-o0H8MAWnvQ3EWA";
 const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/shirshodipto/clc35ntfr005114mp3i4fd3i7',
-    center: [11.172883, 20.720579],
-    zoom: 1.2
+  container: "map",
+  style: "mapbox://styles/shirshodipto/clc35ntfr005114mp3i4fd3i7",
+  center: [11.172883, 20.720579],
+  zoom: 1.2,
 });
 
-for (const input of viewInputs) {
-    input.onclick = (layer) => {
-        changeStyle(layer);
-    };
+// ESLINT
+
+function toggleLayer(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  const clickedLayer = e.target.id;
+  const visibility = map.getLayoutProperty(clickedLayer, "visibility");
+
+  //   const lines2 = e.target.getAttribute("lines2");
+  //   console.log(lines2);
+
+  if (visibility === "visible" || visibility === undefined) {
+    map.setLayoutProperty(clickedLayer, "visibility", "none");
+    this.className = "";
+  } else {
+    this.className = "active";
+    map.setLayoutProperty(clickedLayer, "visibility", "visible");
+  }
 }
 
-
-map.on('idle', () => {
-    allLayers.forEach((layer) => {
-        layer.onclick = toggleLayer;
-        console.log('idle');
-    })
-
-    // if the map has changed style, load 
-    // previous styles layers
-    if (isChangeStyle === 1) {
-        for (const layer of mapIds) {
-            const prevVis = prevLayers[`${layer}`].visibility;
-            console.log(prevVis);
-            map.setLayoutProperty(layer, 'visibility', prevVis);
-        }
-        isChangeStyle = 0;
+function updateLegends(e) {
+  const legendName = e.target.id;
+  const allLegends = document.querySelectorAll("div[dataId]");
+  allLegends.forEach((legend) => {
+    if (legend.getAttribute("dataId") !== legendName) {
+      legend.classList.add("hide");
+    } else {
+      legend.classList.remove("hide");
     }
-});
-
-
-const legendsInputs = document.querySelectorAll('.legends-inputs input');
-
-for (const input of legendsInputs) {
-    input.onclick = updateLegends;
+  });
 }
 
+function displayPopUp(e) {
+  const coordinates = e.features[0].geometry.coordinates.slice();
+  const div = createConflictPopUp(e.features[0].properties);
+
+  while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+    coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+  }
+
+  new mapboxgl.Popup().setLngLat(coordinates).setDOMContent(div).addTo(map);
+}
+
+function changeStyle(layer) {
+  // eslint-disable-next-line no-underscore-dangle
+  prevLayers = map.style._layers;
+  map.setStyle(layer.target.id);
+  isChangeStyle += 1;
+}
+
+// ############ MAIN WORKFLOW #############
+
+viewInputs.forEach((input) => {
+  input.addEventListener("click", (layer) => {
+    changeStyle(layer);
+  });
+});
+
+map.on("idle", () => {
+  allLayers.forEach((layer) => {
+    layer.addEventListener("click", toggleLayer);
+    // console.log("idle");
+  });
+
+  // if the map has changed style, load
+  // previous styles layers
+  if (isChangeStyle === 1) {
+    mapIds.forEach((layer) => {
+      const prevVis = prevLayers[`${layer}`].visibility;
+      map.setLayoutProperty(layer, "visibility", prevVis);
+    });
+    isChangeStyle = 0;
+  }
+});
+
+const legendsInputs = document.querySelectorAll(".legends-inputs input");
+
+legendsInputs.forEach((input) => {
+  input.addEventListener("click", updateLegends);
+});
 
 // add popup features
-map.on('click', 'conflicted-zones', (e) => {
-    displayPopUp(e);
-})
+map.on("click", "conflicted-zones", (e) => {
+  displayPopUp(e);
+});
 
-map.on('mouseenter', 'conflicted-zones', () => {
-    map.getCanvas().style.cursor = 'pointer';
+map.on("mouseenter", "conflicted-zones", () => {
+  map.getCanvas().style.cursor = "pointer";
 });
-        
-map.on('mouseleave', 'conflicted-zones', () => {
-    map.getCanvas().style.cursor = '';
+
+map.on("mouseleave", "conflicted-zones", () => {
+  map.getCanvas().style.cursor = "";
 });
+
+// ##############
--- src/popUp.js
+++ src/popUp.js
@@ -1,84 +1,81 @@
+export default function createConflictPopUp(data) {
+  const div = document.createElement("div");
+  div.className = "pop-up";
 
+  const title = document.createElement("div");
+  title.className = "title";
+  title.textContent = data.territory;
+  div.appendChild(title);
 
-export function createConflictPopUp(data) {
-    
-    const div = document.createElement('div');
-    div.className = 'pop-up';
+  const nature = document.createElement("div");
+  nature.className = "nature";
+  div.appendChild(nature);
 
-    const title = document.createElement('div');
-    title.className = 'title';
-    title.textContent = data.territory;
-    div.appendChild(title);
+  const since = document.createElement("div");
+  since.className = "since";
+  div.appendChild(since);
 
-    const nature = document.createElement('div');
-    nature.className = 'nature';
-    div.appendChild(nature);
+  const deaths = document.createElement("div");
+  deaths.className = "deaths";
+  div.appendChild(deaths);
 
-    const since = document.createElement('div');
-    since.className = 'since';
-    div.appendChild(since);
+  const currentStatus = document.createElement("div");
+  currentStatus.className = "current-status";
+  div.appendChild(currentStatus);
 
-    const deaths = document.createElement('div');
-    deaths.className = 'deaths';
-    div.appendChild(deaths);
+  const impacts = document.createElement("div");
+  impacts.className = "impacts";
+  div.appendChild(impacts);
 
-    const currentStatus = document.createElement('div');
-    currentStatus.className = 'current-status';
-    div.appendChild(currentStatus);
+  const header1 = document.createElement("div");
+  header1.className = "header";
+  header1.textContent = "NATURE OF CONFLICT";
+  nature.appendChild(header1);
 
-    const impacts = document.createElement('div');
-    impacts.className = 'impacts';
-    div.appendChild(impacts);
+  const description1 = document.createElement("div");
+  description1.className = "description";
+  description1.textContent = data.nature_of_conflict;
+  nature.appendChild(description1);
 
-    const header1 = document.createElement('div');
-    header1.className = 'header';
-    header1.textContent = 'NATURE OF CONFLICT';
-    nature.appendChild(header1);
+  const header2 = document.createElement("div");
+  header2.className = "header";
+  header2.textContent = "SINCE";
+  since.appendChild(header2);
 
-    const description1 = document.createElement('div');
-    description1.className = 'description';
-    description1.textContent = data.nature_of_conflict;
-    nature.appendChild(description1);
+  const description2 = document.createElement("div");
+  description2.className = "description";
+  description2.textContent = data.since;
+  since.appendChild(description2);
 
-    const header2 = document.createElement('div');
-    header2.className = 'header';
-    header2.textContent = 'SINCE';
-    since.appendChild(header2);
+  const header3 = document.createElement("div");
+  header3.className = "header";
+  header3.textContent = "DEATHS";
+  deaths.appendChild(header3);
 
-    const description2 = document.createElement('div');
-    description2.className = 'description';
-    description2.textContent = data.since;
-    since.appendChild(description2);
+  const description3 = document.createElement("div");
+  description3.className = "description";
+  description3.textContent = data.deaths;
+  deaths.appendChild(description3);
 
-    const header3 = document.createElement('div');
-    header3.className = 'header';
-    header3.textContent = 'DEATHS';
-    deaths.appendChild(header3);
+  const header4 = document.createElement("div");
+  header4.className = "header";
+  header4.textContent = "CURRENT STATUS";
+  currentStatus.appendChild(header4);
 
-    const description3 = document.createElement('div');
-    description3.className = 'description';
-    description3.textContent = data.deaths;
-    deaths.appendChild(description3);
+  const description4 = document.createElement("div");
+  description4.className = "description";
+  description4.textContent = data.current_status;
+  currentStatus.appendChild(description4);
 
-    const header4 = document.createElement('div');
-    header4.className = 'header';
-    header4.textContent = 'CURRENT STATUS';
-    currentStatus.appendChild(header4);
+  const header5 = document.createElement("div");
+  header5.className = "header";
+  header5.textContent = "NOTABLE IMPACTS";
+  impacts.appendChild(header5);
 
-    const description4 = document.createElement('div');
-    description4.className = 'description';
-    description4.textContent = data.current_status;
-    currentStatus.appendChild(description4);
+  const description5 = document.createElement("div");
+  description5.className = "description";
+  description5.textContent = data.notable_impacts;
+  impacts.appendChild(description5);
 
-    const header5 = document.createElement('div');
-    header5.className = 'header';
-    header5.textContent = 'NOTABLE IMPACTS';
-    impacts.appendChild(header5);
-
-    const description5 = document.createElement('div');
-    description5.className = 'description';
-    description5.textContent = data.notable_impacts;
-    impacts.appendChild(description5);
-
-    return div
+  return div;
 }
